6c548b9dd774c9b0e8e1d5157c7dc122d540d10b
nilteras <niltonrnascimento@hotmail.com>
2023-08-04 12:17:09 -0300

    /POST signUp and signIn config

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { signIn, signUp } from "../controllers/auth.controller.js";
 
 
 const authRouter = Router()
 
-authRouter.post('/signup')
-authRouter.post('/signin')
+authRouter.post('/signup', signUp)
+authRouter.post('/signin', signIn)
 
 export default authRouter
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-
 const usersRouter = Router()
 
 usersRouter.get('/users/me')
